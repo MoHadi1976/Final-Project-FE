663a45d0f9491ddab945cbd79960e2715ff92c09
MoHadi1976 <m_hadi24@hotmail.com>
2021-10-12 10:12:05 +0100

    new repo

--- final-project-hosted-fe/src/components/login/index.js
+++ final-project-hosted-fe/src/components/login/index.js
@@ -7,8 +7,8 @@ const Login = ({setPass, setEmail, loginHandler}) => {
                <a href="/">Homepage</a>
 
         <h1>Login</h1>
-        <input placeholder='email' type="email" onChange={(e) => setEmail(e.target.value) } />
-        <input placeholder='password' type="password" onChange={(e) => setPass(e.target.value)}  />
+        <input classname="input-fields" placeholder='email' type="email" onChange={(e) => setEmail(e.target.value) } />
+        <input classname="input-fields" placeholder='password' type="password" onChange={(e) => setPass(e.target.value)}  />
         <button type='submit'>Login</button>
       </form>
     )
