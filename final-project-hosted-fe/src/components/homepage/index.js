6767c4418901a5072ab81d3c158b5f7d5649e841
MoHadi1976 <m_hadi24@hotmail.com>
2021-10-12 03:13:08 +0100

    initial commit

---
+++ final-project-hosted-fe/src/components/homepage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+ 
+function Homepage() {
+ 
+  return (
+    <div>
+      <br />
+                <a href="/signup">Sign-up</a>
+                <a href="/login">Login</a>
+                <h1>Homepage</h1><br /><br />
+    </div>
+  );
+}
+ 
+export default Homepage;
---
+++ final-project-hosted-fe/src/components/upload/index.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import { useHistory } from "react-router-dom";
+import Alert from '../alert';
+
+export default function Upload() {
+    const history = useHistory();
+    const [fileInputState, setFileInputState] = useState('');
+    const [previewSource, setPreviewSource] = useState('');
+    const [selectedFile, setSelectedFile] = useState();
+    const [successMsg, setSuccessMsg] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const handleFileInputChange = (e) => {
+        const file = e.target.files[0];
+        previewFile(file);
+        setSelectedFile(file);
+        setFileInputState(e.target.value);
+    };
+
+    const handleLogout = () => {  
+     
+        localStorage.clear("myToken");
+        history.push("/login");
+    }
+
+    const previewFile = (file) => {
+        const reader = new FileReader(); //FileReader is a buit in js method
+        reader.readAsDataURL(file); //Convert image to a string
+        reader.onloadend = () => {
+            setPreviewSource(reader.result);
+        };
+    };
+
+    const handleSubmitFile = (e) => {
+        e.preventDefault();
+        if (!selectedFile) return;
+        const reader = new FileReader();
+        reader.readAsDataURL(selectedFile);
+        reader.onloadend = () => {
+            uploadImage(reader.result);
+        };
+        reader.onerror = () => {
+            console.error('AHHHHHHHH!!');
+            setErrMsg('something went wrong!');
+        };
+    };
+
+    const uploadImage = async (base64EncodedImage) => {
+        console.log(base64EncodedImage) //send encoded image string to the console
+        try {
+            await fetch('https://postcard-pals-proxy.herokuapp.com/api/upload', {
+                method: 'POST',
+                body: JSON.stringify({ data: base64EncodedImage }),
+                headers: { 'Content-Type': 'application/json' },
+            });
+            setFileInputState('');
+            setPreviewSource('');
+            setSuccessMsg('Image uploaded successfully');
+        } catch (err) {
+            console.error(err);
+            setErrMsg('Something went wrong!');
+        }
+    };
+    return (
+        <>
+        <input type="button" onClick={handleLogout} value="Logout" />
+        <div>
+            <h1 className="title">Upload an Image</h1>
+            <Alert msg={errMsg} type="danger" />
+            <Alert msg={successMsg} type="success" />
+            {/* this next line calls the function that will handle submitting the file */}
+            <form onSubmit={handleSubmitFile} className="form">    
+                <input
+                    id="fileInput"
+                    type="file"
+                    name="image"
+                    onChange={handleFileInputChange}
+                    value={fileInputState}
+                    className="form-input"
+                />
+                <button className="btn" type="submit">
+                    Submit
+                </button>
+            </form>
+            {previewSource && (     //NOTE: this asks if there is a preview source then use an image tag todisplay it
+                <img                //The height is sey to 300px so it doesn't swamp the page
+                    src={previewSource}
+                    alt="chosen"
+                    style={{ height: '300px' }}
+                />
+            )}
+        </div>
+        </>
+    );
+}
